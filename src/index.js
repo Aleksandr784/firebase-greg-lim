651001bcc69cebc272921b6f4b5ff3a0b5202bb0
Aleksandr784 <digiprotekt@gmale.com>
2021-08-01 08:10:22 +0300

    first-commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as firebase from 'firebase';
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBg2YXT9Zv20R22ABQEtaqp8SxIUeOM8b0",
+    authDomain: "fir-greg-lim.firebaseapp.com",
+    projectId: "fir-greg-lim",
+    storageBucket: "fir-greg-lim.appspot.com",
+    messagingSenderId: "715993323573",
+    appId: "1:715993323573:web:d84b58bd8f3d5b8139797c",
+    measurementId: "G-N9Y682NKW0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
 
 ReactDOM.render(
   <React.StrictMode>
